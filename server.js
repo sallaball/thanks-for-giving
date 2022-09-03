daae2c66cae9c7eb7e00571950f706fb4d3ec216
WeWantSomeMo <mason.oz5@yahoo.com>
2022-09-03 14:54:22 -0500

    updated structure for file and added utils

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const app = express();
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
