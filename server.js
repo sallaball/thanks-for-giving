c6c74fb0a974db582a5e38955a2a39a0822e629e
sallaball <sallaball1206@gmail.com>
2022-09-04 21:24:03 +0300

    updates

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-const sequelizeStore = require('connect-session-sequelize')(session.store);
+const SequelizeStore = require('connect-session-sequelize')(session.store);
 
 const sess = {
     secret: 'SuperSecret',
     cookie: {},
-    resave: false,
+    resave: true,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
@@ -38,5 +38,5 @@ app.use(routes);
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening on port ${PORT}'))
+    app.listen(PORT, () => console.log('Now listening'))
 });
