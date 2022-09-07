5e163ce3153d368fc25e18d48f0367bb207c64bc
Hcuadro2326 <cuadro.bebo2326@gmail.com>
2022-09-06 20:02:15 -0500

    recipes.js

--- models/user.js
+++ models/user.js
@@ -1,17 +1,52 @@
-module.exports = (sequelize, DataTypes) => {
+const {Model,DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-    const User = sequelize.define ('user' ,
+class User extends Model {
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw,this.password)
+    }
+}
+
+User.init(
     {
-        username: DataTypes.STRING,
-        password: DataTypes.STRING
+        username:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[10],
+            },
+        },
     },
     {
-        tableNameFreeze: true
-    });
-    
-    return User;
-};
-
-
-
-//needs the const to rquire models at the top.?
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+    },
+);
