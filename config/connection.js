fe7832f7059cd87a8aac8c5fe1be73105dbd27cf
damiandlg <damian.delagarzaf@gmail.com>
2022-09-10 14:04:44 -0500

    sql not working

--- config/connection.js
+++ config/connection.js
@@ -35,11 +35,11 @@
 
 require("dotenv").config();
 const Sequelize = require("sequelize");
-let sequelize;
-if(process.env.JAWSDB_URL){
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//let sequelize;
+// if(process.env.JAWSDB_URL){
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+//}else {
+  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: "localhost",
     dialect: "mysql",
     dialectOptions:{
@@ -47,5 +47,5 @@ if(process.env.JAWSDB_URL){
     },
     port: 3306,
   });
-}
+//}
 module.exports = sequelize;
--- models/user.js
+++ models/user.js
@@ -1,58 +1,8 @@
-// const {Model,DataTypes} = require('sequelize');
-// const sequelize = require('../config/connection');
-// const bcrypt = require('bcrypt');
+const {Sequelize, Model, DataTypes} = require("sequelize")
+//const sequelize = require('../config/connection');
+const sequelize = new Sequelize("sqlite::memory:");
 
-// class User extends Model {
-//     checkPassword(loginPw){
-//         return bcrypt.compareSync(loginPw,this.password)
-//     }
-// }
 
-// User.init(
-//     {
-//         id:{
-//             type:DataTypes.INTEGER,
-//             allowNull:false,
-//             primaryKey:true,
-//             autoIncrement:true
-//         },
-//         email: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//             unique: true,
-//             validate: {
-//                 isEmail: true,
-//             },
-//         },
-//         password: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//             validate: {
-//                 len:[10],
-//             },
-//         },
-//     },
-//     {
-//         hooks: {
-//             beforeCreate: async (newUserData) => {
-//                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
-//                 return newUserData;
-//             },
-//             beforeUpdate: async (updatedUserData) => {
-//                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-//                 return updatedUserData;
-//             },
-//         },
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'user',
-//     },
-// );
-
-const Sequelize = require('sequelize');
-const db = require('../config/connection');
 
 const bcrypt = require("bcrypt")
 
@@ -65,31 +15,31 @@ class User extends Model{
 User.init(
     {
         id: {
-            type: Sequelize.INTEGER,
+            type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
         user_name: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
             unique: true
         },
         username: {
-            type: Sequelize.INTEGER,
+            type: DataTypes.INTEGER,
             allowNull: false
         },
         email: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false
         },
         password: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false
         },
-        created: {
-            type: Sequelize.DATE,
-            defaultValue: Sequelize.NOW
-        }
+        //created: {
+          //  type: Sequelize.DATE,
+            //defaultValue: Sequelize.NOW
+        //}
     },
     {
         hooks: {
