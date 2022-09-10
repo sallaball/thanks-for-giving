52f493dd10e4fd280465fa40af4b84aa37592302
damiandlg <damian.delagarzaf@gmail.com>
2022-09-10 13:01:12 -0500

    .

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 // const { Post, User, Comment, Vote } = require('../models');
 const withAuth = require('../utils/auth');
-
+//example
 module.exports = router;
