e3987a8907a720140dfffc1ac0f3d185a78f6f9a
sallaball <sallaball1206@gmail.com>
2022-10-30 21:11:03 -0500

    changes for connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: "127.0.0.1",
+require('dotenv').config();
+
+let sequelize;
+
+  if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: "localhost",
     dialect: "mysql",
     dialectOptions:{
       decimalNumbers: true,
     },
     port: 3306,
   });
+}
 module.exports = sequelize;
